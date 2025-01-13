1f38a926cab76d4c8c4c1605fb210afabb63e1ee
mariana <marianafmz18@gmail.com>
2025-01-13 16:01:10 -0600

    Modificaciòn models usuarios

--- capacitaciondoc/usuarios/models.py
+++ capacitaciondoc/usuarios/models.py
@@ -1,6 +1,30 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
- 
+
+#Agregado para crear un administrador de usuarios, esto permite que cuando creemos un superusuario no se solicite el nombre de usuario
+#pero solicita CURP y será asignado el CURP como el nombre de usuario automaticamente
+class CustomUserManager(BaseUserManager):
+    def create_user(self, curp, password=None, **extra_fields):
+        if not curp:
+            raise ValueError('El campo CURP debe ser proporcionado.')
+        extra_fields.setdefault('is_active', True)
+        user = self.model(curp=curp, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, curp, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('El superusuario debe tener is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('El superusuario debe tener is_superuser=True.')
+
+        return self.create_user(curp, password, **extra_fields)
+    
+
 class CustomUser(AbstractUser):
     last_name_paterno = models.CharField(max_length=50, verbose_name="Apellido paterno")
     last_name_materno = models.CharField(max_length=50, verbose_name="Apellido materno")
@@ -10,12 +34,14 @@ class CustomUser(AbstractUser):
         choices=[('Docente', 'Docente'), ('Instructor', 'Instructor')],
         verbose_name="Rol"
     )
-   
+
     USERNAME_FIELD = 'curp'  #Agregado: Define al CURP como el identificador inicial para permitir el Login con CURP
+    objects = CustomUserManager()  # Asigna el nuevo UserManager
+    
     def save(self, *args, **kwargs):
         # Asignar el valor de CURP al campo username
         self.username = self.curp
         super().save(*args, **kwargs)  # Llamar al método save del modelo base
- 
-    def __str__(self):
-        return f"{self.first_name} {self.last_name_paterno} {self.last_name_materno} - {self.rol}"
+
+    def _str_(self):
+        return f"{self.first_name} {self.last_name_paterno} {self.last_name_materno} - {self.rol}"
