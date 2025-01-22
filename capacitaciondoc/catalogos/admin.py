e4985c5251db04092311e335813a79b4eed57a4a
FerTecnm <garciafernanda1im@gmail.com>
2025-01-22 01:23:18 -0600

    Se elimina asignacion de permisos basados en roles de admin.py por conflicto de migraciones; se mueve a apps.py para asignar permisos en un postmigrate

--- capacitaciondoc/catalogos/admin.py
+++ capacitaciondoc/catalogos/admin.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
-from django.contrib.auth.models import Group, Permission
 
 from .models import (
     ExperienciaDocente, 
@@ -91,53 +90,4 @@ class HoraAdmin(admin.ModelAdmin):
     search_fields = ('cantidad',)
 
 
-# Permisos
-def configurar_grupos_y_permisos():
-    # Crea o obtiene los grupos en la BD
-    docente_group, created = Group.objects.get_or_create(name='Docente')
-    instructor_group, created = Group.objects.get_or_create(name='Instructor')
-    academico_group, created = Group.objects.get_or_create(name='Jefe Académico')
-    capacitacion_group, created = Group.objects.get_or_create(name='Jefe de Capacitación')
-    subdireccion_group, created = Group.objects.get_or_create(name='Subdirección')
 
-    # Obtener permisos definidos en los modelos
-    permission_view_instructor = Permission.objects.get(codename='view_instructor')
-    permission_edit_instructor = Permission.objects.get(codename='change_instructor')
-    permission_delete_instructor = Permission.objects.get(codename='delete_instructor')
-    permission_add_instructor = Permission.objects.get(codename='add_instructor')
-
-    permission_view_docente = Permission.objects.get(codename='view_docente')
-    permission_edit_docente = Permission.objects.get(codename='change_docente')
-    permission_delete_docente = Permission.objects.get(codename='delete_docente')
-    permission_add_docente = Permission.objects.get(codename='add_docente')
-
-    # Asignar permisos a los grupos
-    # Grupo Instructor: solo puede ver y editar instructores
-    instructor_group.permissions.add(permission_view_instructor, permission_edit_instructor)
-
-    # Grupo Docente: solo puede ver y editar docentes
-    docente_group.permissions.add(permission_view_docente, permission_edit_docente)
-
-    # Grupo Jefe Académico: puede ver instructores y docentes, pero no editarlos
-    academico_group.permissions.add(permission_view_instructor, permission_view_docente)
-
-    # Grupo Jefe Capacitación: puede ver, agregar, modificar y eliminar instructores y docentes
-    capacitacion_group.permissions.add(
-        permission_view_instructor,
-        permission_edit_instructor,
-        permission_add_instructor,
-        permission_delete_instructor,
-        permission_delete_docente,
-        permission_add_docente,
-        permission_view_docente,
-        permission_edit_docente,
-    )
-
-    # Guardar los grupos
-    instructor_group.save()
-    docente_group.save()
-    academico_group.save()
-    capacitacion_group.save()
-
-# Llamar a la función para configurar grupos y permisos
-configurar_grupos_y_permisos()
--- capacitaciondoc/catalogos/apps.py
+++ capacitaciondoc/catalogos/apps.py
@@ -1,6 +1,61 @@
 from django.apps import AppConfig
-
+from django.db.models.signals import post_migrate
 
 class CatalogosConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'catalogos'
+
+    def ready(self):
+        
+        from django.contrib.auth.models import Group, Permission
+        from django.db.models.signals import post_migrate
+        from django.core.exceptions import ObjectDoesNotExist
+
+        # Permisos
+        def configurar_grupos_y_permisos(sender, **kwargs):
+            # Crea o obtiene los grupos en la BD
+            docente_group, created = Group.objects.get_or_create(name='Docente')
+            instructor_group, created = Group.objects.get_or_create(name='Instructor')
+            academico_group, created = Group.objects.get_or_create(name='Jefe Académico')
+            capacitacion_group, created = Group.objects.get_or_create(name='Jefe de Capacitación')
+            subdireccion_group, created = Group.objects.get_or_create(name='Subdirección')
+
+            try:
+                # Obtener permisos definidos en los modelos
+                permission_view_instructor = Permission.objects.get(codename='view_instructor')
+                permission_edit_instructor = Permission.objects.get(codename='change_instructor')
+                permission_delete_instructor = Permission.objects.get(codename='delete_instructor')
+                permission_add_instructor = Permission.objects.get(codename='add_instructor')
+
+                permission_view_docente = Permission.objects.get(codename='view_docente')
+                permission_edit_docente = Permission.objects.get(codename='change_docente')
+                permission_delete_docente = Permission.objects.get(codename='delete_docente')
+                permission_add_docente = Permission.objects.get(codename='add_docente')
+
+                # Asignar permisos a los grupos
+                # Grupo Instructor: solo puede ver y editar instructores
+                instructor_group.permissions.add(permission_view_instructor, permission_edit_instructor)
+
+                # Grupo Docente: solo puede ver y editar docentes
+                docente_group.permissions.add(permission_view_docente, permission_edit_docente)
+
+                # Grupo Jefe Académico: puede ver instructores y docentes, pero no editarlos
+                academico_group.permissions.add(permission_view_instructor, permission_view_docente)
+
+                # Grupo Jefe Capacitación: puede ver, agregar, modificar y eliminar instructores y docentes
+                capacitacion_group.permissions.add(
+                    permission_view_instructor,
+                    permission_edit_instructor,
+                    permission_add_instructor,
+                    permission_delete_instructor,
+                    permission_delete_docente,
+                    permission_add_docente,
+                    permission_view_docente,
+                    permission_edit_docente,
+                )
+            except Permission.DoesNotExist as e:
+                # Mostrar un error detallado en la consola si algún permiso no existe
+                print(f"Error al configurar permisos: {e}")
+        
+        # Conectar la señal post_migrate
+        post_migrate.connect(configurar_grupos_y_permisos, sender=self)
