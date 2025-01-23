b685f7533dea3f8e31519c7f0e452947cfc688b2
FerTecnm <garciafernanda1im@gmail.com>
2025-01-23 12:53:50 -0600

    Se elimina formulario duplicado

--- capacitaciondoc/plancapacitacion/forms.py
+++ capacitaciondoc/plancapacitacion/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from .models import registroCurso
 
-class EditarCursoForm(forms.ModelForm):
+class CursoForm(forms.ModelForm):
     class Meta:
         model = registroCurso
         #En lugar de poner todos los campos con fields = '__all__', pongo exlude y se pone el campo que no quiero que se muestre
@@ -16,16 +16,3 @@ class EditarCursoForm(forms.ModelForm):
         for field in iter(self.fields):
             self.fields[field].widget.attrs.update({'class': 'form-control'}) 
 
-class AñadirCursoForm(forms.ModelForm):
-    class Meta:
-        model= registroCurso
-        exclude = ['aceptado'] 
-        labels={'nombre': 'Nombre del cuso', 
-                'periodo': 'Periodo del curso',  
-                'dirigido': 'Dirigido a', 
-                'perfilCurso': 'Perfil del curso'}
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        for field in iter(self.fields):
-            self.fields[field].widget.attrs.update({'class': 'form-control'}) 
