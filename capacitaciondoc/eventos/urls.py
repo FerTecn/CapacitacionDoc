00972c1a3d5ad90dca5cb9c2ddbc3447821ceed0
FerTecnm <garciafernanda1im@gmail.com>
2025-01-28 23:26:57 -0600

    Se configura rutas

--- capacitaciondoc/eventos/urls.py
+++ capacitaciondoc/eventos/urls.py
@@ -6,13 +6,18 @@ urlpatterns = [
     path('eventolista/', views.eventolista, name='eventolista'),
     path('crearevento/<int:evento_id>/', views.crearevento, name='crearevento'),
     path('eventover/<int:evento_id>/', views.eventover, name='eventover'),
-    path('añadirlugar/', views.añadirlugar, name='añadirlugar'), 
+    path('eventodeshacer/<int:evento_id>/', views.eventodeshacer, name='eventodeshacer'),
+
+    path('lugarcrearnuevo/', views.lugarcrearnuevo, name='lugarcrearnuevo'), 
     path('evento/<int:evento_id>/cambiarinstructor/', views.cambiarinstructor, name='cambiarinstructor'), 
     path('evento/<int:evento_id>/añadirinstructor/', views.añadirinstructor, name='añadirinstructor'),
     
     #Inscripcion
     path('inscripcionlista/', views.inscripcionlista, name='inscripcionlista'),
     path('vercurso/<int:evento_id>/', views.vercurso, name='vercurso'),
-    path('aceptar/<int:inscripcion_id>/', views.aceptarinscripcion, name='aceptarinscripcion'),
-    path('invalidar/<int:inscripcion_id>/', views.invalidarinscripcion, name='invalidarinscripcion'),
+    path('aceptarinscripcion/<int:evento_id>/', views.aceptarinscripcion, name='aceptarinscripcion'),
+    path('invalidar/<int:evento_id>/', views.invalidarinscripcion, name='invalidarinscripcion'),
+
+    #Cursos
+    path('miscursos/', views.miscursosinstructor, name='miscursos'),
 ]
