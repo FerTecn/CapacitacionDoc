6e021afa475524645f29e121c4547a6409f25031
FerTecnm <garciafernanda1im@gmail.com>
2025-01-28 23:25:17 -0600

    Se agrega Form de Evento

---
+++ capacitaciondoc/eventos/forms.py
@@ -0,0 +1,34 @@
+from django import forms
+from .models import Evento
+
+class EventoForm(forms.ModelForm):
+    class Meta:
+        model = Evento
+        fields = ['fechaInicio', 'fechaFin', 'horaInicio', 'horaFin', 'lugar']
+        labels ={
+            'fechaInicio': 'Fecha de inicio',
+            'fechaFin': 'Fecha de culminación',
+        }
+        widgets = {
+            'fechaInicio': forms.DateInput(attrs={'type': 'date', 'class': 'form-control'}, format='%Y-%m-%d'),
+            'fechaFin': forms.DateInput(attrs={'type': 'date', 'class': 'form-control'}, format='%Y-%m-%d'),
+            'horaInicio': forms.TimeInput(attrs={'type': 'time', 'class': 'form-control'}),
+            'horaFin': forms.TimeInput(attrs={'type': 'time', 'class': 'form-control'}),
+            'lugar': forms.Select(attrs={'class': 'form-control'}),
+        }
+    def clean(self):
+        cleaned_data = super().clean()
+        fechaInicio = cleaned_data.get('fechaInicio')
+        fechaFin = cleaned_data.get('fechaFin')
+        horaInicio = cleaned_data.get('horaInicio')
+        horaFin = cleaned_data.get('horaFin')
+
+        # Validar fechas
+        if fechaInicio and fechaFin and fechaInicio > fechaFin:
+            raise forms.ValidationError("La fecha de inicio no puede ser posterior a la fecha de fin.")
+
+        # Validar horas
+        if horaInicio and horaFin and horaInicio >= horaFin:
+            raise forms.ValidationError("La hora de inicio debe ser menor que la hora de fin.")
+
+        return cleaned_data
