40bc524cfe452dca8b6ae61240241d676211e754
FerTecnm <garciafernanda1im@gmail.com>
2025-01-31 13:46:37 -0600

    Se agrega url de encuesta y del listado de cursos

--- capacitaciondoc/encuesta/urls.py
+++ capacitaciondoc/encuesta/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('encuesta/', views.encuesta, name='encuesta'),
+    path('encuesta/', views.listarcursos4encueta, name='encuesta'),
+    path('encuesta/<int:curso_id>', views.encuesta, name='realizarencuesta'),
     path('gracias/', views.gracias, name='gracias'),
 ]
