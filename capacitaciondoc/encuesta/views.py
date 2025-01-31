5e70cf2f65f056003df7de470e1fce363e6633bc
FerTecnm <garciafernanda1im@gmail.com>
2025-01-31 13:43:21 -0600

    Se agrega vista para listar cursos del que hará encuesta. Se adapta la vista de la encuesta para guardar los campos docente, evento

--- capacitaciondoc/encuesta/views.py
+++ capacitaciondoc/encuesta/views.py
@@ -1,10 +1,54 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
+
+from eventos.models import Inscripcion, Evento
+from catalogos.models import Docente
+from plancapacitacion.models import RegistroCurso
 from .models import Encuesta
+from django.contrib import messages
 
 # Create your views here.
-def encuesta(request):
+def listarcursos4encueta(request):
+    # Filtra las inscripciones del usuario docente autenticado
+    inscripciones = Inscripcion.objects.filter(usuario=request.user)
+    cursos = []
+    for inscripcion in inscripciones:
+        evento = inscripcion.evento
+        curso = evento.curso
+
+        # Verificar si ya tiene una encuesta existente relacionada
+        encuesta_hecha = Encuesta.objects.filter(docente=request.user, curso= curso).exists()
+
+        cursos.append({
+            'evento': evento,
+            'curso': curso,
+            'encuesta_hecha': encuesta_hecha,
+        })
+    return render(request, 'cursosparaencuesta.html', {'cursos': cursos})
+
+    
+def encuesta(request, curso_id):
+    # Obtener el evento especifico
+    curso = get_object_or_404(RegistroCurso, id=curso_id)
+    evento = get_object_or_404(Evento, curso=curso)
+
+    if Encuesta.objects.filter(docente=request.user, curso=curso).exists():
+        messages.warning(request, 'Ya realizaste esta encuesta')
+        return redirect('encuesta')
+    
     if request.method == 'POST':
+        # Verificar si el docente estuvo inscrito en el curso
+        inscripcion_valida = Inscripcion.objects.filter(
+            usuario=request.user,
+            evento__curso=curso,
+            aceptado=True,
+        ).exists()
+
+        if not inscripcion_valida:
+            print('invalido')
+            return redirect('encuesta')
+        
         encuesta = Encuesta(
+            docente = request.user, curso = curso,
             instructor1=request.POST['instructor1'],
             instructor2=request.POST['instructor2'],
             instructor3=request.POST['instructor3'],
@@ -33,7 +77,7 @@ def encuesta(request):
         )
         encuesta.save()
         return redirect('gracias')  # Redirige a una página de agradecimiento
-    return render(request, 'encuesta.html')
+    return render(request, 'encuesta.html', {'evento': evento})
 
 def gracias(request):
     return render(request, 'gracias.html')
